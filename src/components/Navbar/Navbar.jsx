a5d16caf213118b96cbccb37a143b699a75aeb37
Matthew Ng <nglingshan30@gmail.com>
2023-01-03 01:00:33 -0700

    Imported Starters Code

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import './Navbar.css';
+import images from '../../constants/images';
+
+const Navbar = () => (
+  <nav className='app__navbar'>
+    Navbar
+  </nav>
+);
+
+export default Navbar;
